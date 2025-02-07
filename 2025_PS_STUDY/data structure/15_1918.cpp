65186a6574030dd423f1dd08edbf2dde3f15ea28
daybreaker42 <june12300039@gmail.com>
2025-02-08 07:36:51 +0900

    1918

--- 2025_PS_STUDY/data structure/15_1918.cpp
+++ 2025_PS_STUDY/data structure/15_1918.cpp
@@ -7,79 +7,105 @@ using namespace std;
 
 // 연산자 우선순위 지정
 map<char, int> priority = {
+    {'(', 3},
+    {')', 3},
     {'*', 2},
     {'/', 2},
     {'+', 1},
     {'-', 1},
 };
 
-// 해당 charactor가 피연산자인지 연산자인지 구분
-bool isCharactor(char c){
-    if('A' <= c && c <= 'Z') return true;
-    return false;
+string result;
+stack<string> variable;
+stack<char> oper;
+
+void operStart() {
+    string v1 = variable.top(), v2;
+    variable.pop();
+    v2 = variable.top();
+    variable.pop();
+    variable.push(v2 + v1 + oper.top());
+    oper.pop();
 }
 
-string handleBrakets(string& exp, string& result, int idx){
-    stack<char> operStack;
-    queue<char> operandQ;
-    int length = exp.length();
-    while(idx < length){
-        char targetCh = exp[idx];
+int main() {
+    fastio;
 
-        if(idx = length - 1){   // 마지막 문자인 경우
-            
+    freopen("input.txt", "r", stdin);
+
+    // for(int t = 0; t < 100; t++){
+
+    string exp;
+    cin >> exp;
+
+    // string ans;
+    // cin >> exp >> ans >> ans;
+
+    // solving
+
+    for (int i = 0; i < exp.length(); i++) {
+        char c = exp[i];
+        if (isalpha(c)) {
+            variable.push({ c });
         }
+        else {
+            if (!oper.empty() && priority[oper.top()] >= priority[c]) {
+                while(!oper.empty() && priority[oper.top()] >= priority[c]){
+                    if (oper.top() == '(') {
+                        break;
+                    }
+                    operStart();
+                }
 
-        if(isCharactor(targetCh)){  // 문자가 들어오는 경우
-            operandQ.push(targetCh);
-        }else if(targetCh == '('){  // 괄호를 만나는 경우
-            string tempResult = handleBrakets(exp, result, idx + 1);
-            result += tempResult;
-        }else if(targetCh == ')'){  // 괄호가 종료되는 경우
-            return result;
-        }else{
-            // 괄호 외 연산자를 만나는 경우
-            if(operStack.empty()){  // stack이 비어있는 경우
-               operStack.push(targetCh); 
-            }else{
-                // stack에 이미 연산자가 있는 경우
-                if(priority[operStack.top()] >= priority[targetCh]){        // 1) stack에 있는게 연산순위가 더 높거나 같을 때
-                    result += operandQ.front();
-                    operandQ.pop();
-                    result += operandQ.front() + operStack.top();
-                    operandQ.pop();
-                    operStack.pop();
-
-                    operStack.push(targetCh);
-                }else if(priority[operStack.top()] < priority[targetCh]){   // 2) stack에 있는게 연산순위가 더 낮을 때
-                    operStack.push(targetCh);
+                if(c == ')'){
+                    oper.pop();
+                }else{
+                    oper.push(c);
+                }
+            }
+            else if (oper.empty()) {
+                // oper가 비어있는 경우
+                oper.push(c);
+            }
+            else {
+                // oper에 값이 있는데 c가 그것보다 우선순위가 높을 경우
+                if (c == ')') {
+                    do {
+                        operStart();
+                    } while (oper.top() != '(');
+                    oper.pop();
+                }
+                else {
+                    oper.push(c);
                 }
             }
         }
-        idx++;
     }
-    return result;
-};
 
-int main() {
-	fastio;
+    while (!oper.empty()) {
+        operStart();
+    }
 
-	// freopen("input.txt", "r", stdin);
-    string exp;
-    getline(cin, exp);
-    
-    // solving
+//     cout << "t: " << t << "\n";
+    cout << variable.top() << endl;
+//     if(!variable.top().compare(ans)){
+//         cout <<  "True\n";
+//     }else{
+//         cout <<  "False\n";
+//     }
+//     cout << "---------------------\n";
+//     cout << "\n";
+// }
 
-    string result;
-    result = handleBrakets(exp, result, 0);
-    cout << result;
-
-	return 0;
+    return 0;
 }
 
-/* comments 
+/* comments
 idea)
 - 재귀함수를 이용해 괄호 처리 - 괄호 안에서 일어난 연산은 우선적으로 처리되므로
     - 괄호가 끝날 때 내에 들어온 상태인진 검사 X - 괄호가 있어서 들어왔을거기 때문에
 
+
+바로 처리할 수 있는 경우
+다음
 */
--- test.cpp
+++ test.cpp
@@ -2,21 +2,16 @@
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
 
-
+void printStr(string str){
+    cout << str << endl;
+}
 
 int main() {
     fastio;
-    
-    pair<int, int> p1, p2;
-    p1 = {1, 3};
-    p2 = p1;
-    cout << p1.first << " : " << p1.second << "\n";
-    cout << p2.first << " : " << p2.second << "\n";
 
-    p1.first = 5;
-
-    cout << p1.first << " : " << p1.second << "\n";
-    cout << p2.first << " : " << p2.second << "\n";
-
-    cout << 
+    char c = 'a';
+    string s = "hello";
+    s += c;
+    cout << s << endl;
+    printStr({c});
 }
