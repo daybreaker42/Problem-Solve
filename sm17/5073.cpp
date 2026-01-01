efafc6dd9de44c651d92596660204c7afe2daa7f
daybreaker42 <june12300039@gmail.com>
2026-01-01 23:31:42 +0900

    sm17 폴더 추가

---
+++ sm17/5073.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+// const bool debug = false;
+
+int main() {
+	fastio;
+
+	// freopen("../../input.txt", "r", stdin);
+
+	// system("pause");
+	return 0;
+}
+
+/* comment 
+
+
+
+*/
